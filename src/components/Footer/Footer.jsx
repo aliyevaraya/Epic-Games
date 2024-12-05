2ad8d6c822321d88b8760d985b85e1fc57e753da
Raya Aliyeva <73crd54@code.edu.az>
2024-12-05 14:15:55 +0400

    responsive style added to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import { LiaCaretSquareUp } from "react-icons/lia";
 
 function Footer() {
   return (
-    <div className="bg-[#202020] text-white  py-[1em] px-[1.5em] leading-[1.5rem]">
-      <div className="flex justify-between items-center text-[#ccc] text-[1.75em] px-[15px]">
+    <footer className="bg-[#202020] text-white  p-[2em] lg:px-[3em] leading-[1.5rem]">
+      <div className="flex justify-between items-center text-[#ccc] text-[1.75em]">
         <div className="flex gap-3">
           <FaFacebook className="hover:text-[#0078f2] trans" />
           <FaTwitter className="hover:text-[#0078f2] trans" />
@@ -14,108 +14,114 @@ function Footer() {
           <LiaCaretSquareUp className="hover:text-[#0078f2] trans" />
         </div>
       </div>
-      <div className="text-[#e7e7e7] text-[.86em] font-semibold px-[15px] py-5">
+      <div className="text-[#e7e7e7] text-[.86em] font-semibold py-5">
         <div className="pb-4">
-          <span className="text-[#ccc] opacity-[.4] text-[.9rem]">
-            Resources
-          </span>
-          <ul>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Support-A-Creator
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Distribute on Epic Games
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Careers
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Company
-              </a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Fan Art Policy
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                UX Research
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Store EULA
-              </a>
-            </li>
-          </ul>
-          <ul className="mt-2">
-            <li>
-              <a className="hoverBlue trans" href="">
-                Online Services
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Community Rules
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Epic Newsroom
-              </a>
-            </li>
-          </ul>
+          <div className="text-[#ccc] opacity-[.4] text-[.9rem]">Resources</div>
+          <div className="xxs:flex">
+            <ul className="pr-[24px]">
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Support-A-Creator
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Distribute on Epic Games
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Company
+                </a>
+              </li>
+            </ul>
+            <div className=" xs:flex">
+              <ul className=" pr-[24px]">
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    Fan Art Policy
+                  </a>
+                </li>
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    UX Research
+                  </a>
+                </li>
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    Store EULA
+                  </a>
+                </li>
+              </ul>
+              <ul className="mt-2 xs:mt-0">
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    Online Services
+                  </a>
+                </li>
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    Community Rules
+                  </a>
+                </li>
+                <li>
+                  <a className="hoverBlue trans" href="">
+                    Epic Newsroom
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="py-4">
-          <span className="text-[#ccc] opacity-[.4] text-[.9rem]">
+          <div className="text-[#ccc] opacity-[.4] text-[.9rem]">
             Made By Epic Games
-          </span>
-          <ul>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Battle Breakers
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Fortnite
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Infinity Blade
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Robo Recall
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Shadow Complex
-              </a>
-            </li>
-            <li>
-              <a className="hoverBlue trans" href="">
-                Unreal Tournament
-              </a>
-            </li>
-          </ul>
+          </div>
+          <div className="flex">
+            <ul className="pr-4">
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Battle Breakers
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Fortnite
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Infinity Blade
+                </a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Robo Recall
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Shadow Complex
+                </a>
+              </li>
+              <li>
+                <a className="hoverBlue trans" href="">
+                  Unreal Tournament
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <hr className="border border-[#333]" />
-      <div className="text-[.71em] leading-[2em] text-[#ccc] p-4">
+      <div className="w-full xl:w-1/2 text-[.71em] leading-[2em] text-[#ccc] py-4">
         <p>
           © 2024, Epic Games, Inc. All rights reserved. Epic, Epic Games, the
           Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine,
@@ -132,17 +138,23 @@ function Footer() {
           damage that may arise from your use of them.
         </p>
       </div>
-      <div className="text-center">
-        <div className="text-[.75em] text-[#e7e7e7] my-4">
+      <div className="text-center md:flex items-center justify-between">
+        <div className="md:flex items-center gap-4 text-[.75em] text-[#e7e7e7] my-4">
           <p className="hoverBlue trans">Terms of Service</p>
           <p className="hoverBlue trans">Privacy Policy</p>
           <p className="hoverBlue trans">Store Refund Policy</p>
         </div>
         <div className="flex justify-center py-3">
-          <img width="65px" src="images/footer-logo.png" alt="epic games and unreal engine" />
+          <a href="">
+            <img
+              width="65px"
+              src="images/footer-logo.png"
+              alt="epic games and unreal engine"
+            />
+          </a>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
